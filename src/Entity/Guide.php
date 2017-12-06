f88a46ba54f4e674ec02f5dbc099844bbb2ee6d5
Mickael Matuchak <mickael.matuchak@etudiant.univ-lille1.fr>
2017-12-06 16:36:52 +0100

    Mise en place d'API Platform

--- src/Entity/Guide.php
+++ src/Entity/Guide.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="guide")
+ * @ApiResource()
  */
 class Guide
 {
