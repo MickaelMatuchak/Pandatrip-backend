8b0fdcf324c79d2af45efdb848a66d35950a322e
Mickael Matuchak <mickael.matuchak@etudiant.univ-lille1.fr>
2018-02-01 17:53:12 +0100

    Oublie : annotation pour retourner le nom de famille d'un guide

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -55,7 +55,7 @@ class User implements UserInterface, EquatableInterface
     /**
      * @ORM\Column(type="string", length=100)
      * @Assert\NotBlank
-     * @Groups({"user", "visitguide"})
+     * @Groups({"user", "guide", "visitguide"})
      */
     private $lastname;
 
